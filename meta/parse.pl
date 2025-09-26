0aef45f2b0d06c1f5943084c604298e0061fc1ef
Jimmy Jin <jjin62@yahoo.com>
2025-09-26 11:26:32 -0700

    Revert "Support OTN attenuator and amplifier"

--- meta/parse.pl
+++ meta/parse.pl
@@ -96,7 +96,6 @@ my %ATTR_TAGS = (
         "relaxed"        , \&ProcessTagRelaxed,
         "isresourcetype" , \&ProcessTagIsRecourceType,
         "deprecated"     , \&ProcessTagDeprecated,
-        "precision"      , \&ProcessTagPrecision,
         );
 
 my %options = ();
@@ -423,17 +422,6 @@ sub ProcessTagDeprecated
     return undef;
 }
 
-sub ProcessTagPrecision
-{
-    my ($precision, $value, $val) = @_;
-
-    # allow only integers >= 0
-    return $val if $val =~ /^\d+$/;
-
-    LogError "precision tag value '$val', expected an integer >= 0";
-    return undef;
-}
-
 sub ProcessTagRange
 {
     my ($type, $attrName, $value) = @_;
@@ -526,7 +514,7 @@ sub ProcessDescription
 
     return if scalar@order == 0;
 
-    my $rightOrder = 'type:flags(:objects)?(:allownull)?(:allowempty)?(:isvlan)?(:default)?(:range)?(:condition|:validonly)?(:relaxed)?(:isresourcetype)?(:deprecated)?(:precision)?';
+    my $rightOrder = 'type:flags(:objects)?(:allownull)?(:allowempty)?(:isvlan)?(:default)?(:range)?(:condition|:validonly)?(:relaxed)?(:isresourcetype)?(:deprecated)?';
 
     my $order = join(":",@order);
 
@@ -2365,21 +2353,6 @@ sub ProcessNextRelease
     return "true";
 }
 
-sub ProcessPrecision {
-    my ($stat, $precision) = @_;
-
-    # Default 0 if precision is not provided
-    return 0 unless defined $precision;
-
-    # Must be an integer >= 0
-    if ($precision =~ /^\d+$/) {
-        return int($precision);
-    }
-
-    LogError "Unsupported precision value '$precision'. Expected an integer >= 0";
-    return undef;
-}
-
 sub ProcessSingleObjectType
 {
     my ($typedef, $objecttype) = @_;
@@ -2443,7 +2416,6 @@ sub ProcessSingleObjectType
         my $isrelaxed       = ProcessRelaxedType($attr, $meta{relaxed});
         my $apiversion      = ProcessApiVersion($attr);
         my $nextrelease     = ProcessNextRelease($attr);
-        my $precision       = ProcessPrecision($attr, $meta{precision});
 
         my $ismandatoryoncreate = ($flags =~ /MANDATORY/)       ? "true" : "false";
         my $iscreateonly        = ($flags =~ /CREATE_ONLY/)     ? "true" : "false";
@@ -2505,7 +2477,6 @@ sub ProcessSingleObjectType
         WriteSource ".iscustom                      = ($attr >= 0x10000000) && ($attr < 0x20000000),";
         WriteSource ".apiversion                    = $apiversion,";
         WriteSource ".nextrelease                   = $nextrelease,";
-        WriteSource ".valueprecision                = $precision,";
 
         WriteSource "};";
 
